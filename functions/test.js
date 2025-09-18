0dc31d09dd001d92e92d20c1701bc3f914f22d44
cole fidelman <colton.fidd@gmail.com>
2025-09-18 07:47:25 -0700

    Production ready: Remove test functions, keep only core AI email system

--- functions/test.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/plain'
-    },
-    body: 'Function is working!'
-  };
-};
